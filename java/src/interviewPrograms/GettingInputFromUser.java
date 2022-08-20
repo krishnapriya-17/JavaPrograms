33a9b0f2c1f27976528d6f2164a67565c147feb5
Naveen <Naveen@DESKTOP-M2NF5SB>
2022-08-20 22:08:31 +0530

    getting inputs from user

---
+++ java/src/interviewPrograms/GettingInputFromUser.java
@@ -0,0 +1,25 @@
+package interviewPrograms;
+
+import java.util.Scanner;
+
+public class GettingInputFromUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+
+		String name=s.nextLine();
+		
+	
+		System.out.println("Entered "+name);
+
+		Scanner s1=new Scanner(System.in);
+		
+		
+		int date=s.nextInt();
+		System.out.println("Entered "+date);
+		
+		
+	}
+
+}
